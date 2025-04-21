41631f326f99fee80f0677960d54d1b60aee1e62
pete <thinhnguyenngoc0104@gmail.com>
2025-04-21 09:18:47 +0700

    init project

---
+++ src/main/java/com/thingg/ecommerce/SpringApiEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.thingg.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thingg/ecommerce/SpringApiEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thingg.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
